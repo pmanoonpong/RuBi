72d7bac38151744d147c9da3c716769cb10410b2
Jorge Rodriguez <veimox@gmail.com>
2016-01-23 09:59:17 +0100

    Updated

--- src/scripts/installFirstTime.sh
+++ src/scripts/installFirstTime.sh
@@ -18,13 +18,18 @@ echo "    Adding the Legs files to the bashrc"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR
 cd ..
-LEGS_SRC_FOLDER=$(pwd)
+LEGS_FOLDER=$(pwd)
 echo "
+
+### Owns ###
+export LEGS=\"$LEGS_FOLDER\"
+
+### ROS ###
+source \$LEGS/devel/setup.bash
 
 ### Gazebo ###
 source /usr/share/gazebo/setup.sh
-export LEGS_SRC=\"$LEGS_SRC_FOLDER\"
-export GAZEBO_MODEL_PATH=\${GAZEBO_MODEL_PATH}:\$LEGS_SRC/gazebo/models
-export GAZEBO_RESOURCE_PATH=\${GAZEBO_RESOURCE_PATH}:\$LEGS_SRC/gazebo" >> $HOME/.bashrc
+export GAZEBO_MODEL_PATH=\${GAZEBO_MODEL_PATH}:\$LEGS/src:\$LEGS/src/gazebo/models
+export GAZEBO_RESOURCE_PATH=\${GAZEBO_RESOURCE_PATH}:\$LEGS/src/gazebo" >> $HOME/.bashrc
 
 echo "Done! :)"
