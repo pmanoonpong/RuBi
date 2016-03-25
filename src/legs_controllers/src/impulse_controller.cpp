986079fd4ed6f9a97167518323b9b4cacb877ff2
Jorge Rodriguez <veimox@gmail.com>
2016-03-25 15:36:45 +0100

    changed qt creator files from dacbot to legs and added impulse controller

---
+++ src/legs_controllers/src/impulse_controller.cpp
@@ -0,0 +1,3 @@
+#include "impulse_controller.h"
+
+impulse_controller::impulse_controller() {}
---
+++ src/legs_controllers/src/impulse_controller.h
@@ -0,0 +1,9 @@
+#ifndef IMPULSE_CONTROLLER_H
+#define IMPULSE_CONTROLLER_H
+
+class impulse_controller {
+ public:
+  impulse_controller();
+};
+
+#endif  // IMPULSE_CONTROLLER_H
