303c88c44fa90f84b6f1481d6d979593d41a08e1
Jorge Rodriguez <veimox@gmail.com>
2016-03-25 18:17:57 +0100

    added node and service to restart simulation and start with the init positions

--- src/legs_controllers/src/impulse_controller.cpp
+++ src/legs_controllers/src/impulse_controller.cpp
@@ -1,5 +1,184 @@
 #include "impulse_controller.h"
 
-impulse_controller::impulse_controller() {}
+/**
+ * System signal
+ */
+bool killed(false);
+void killerHandler(int sig) { killed = true; }
+/**
+ * @brief Runs the controller
+ */
+int main() {
+  // Init
+  int argc(0);
+  char **argv(NULL);
+  ros::init(argc, argv, "impulse_controller",
+            ros::init_options::NoSigintHandler);
 
+  // Controller
+  ImpulseController impulseController;
 
+  // Signal
+  signal(SIGINT, killerHandler);
+
+  // Step until killed
+  while (!killed) {
+    impulseController.step();
+  }
+
+  impulseController.stop();
+  ros::shutdown();
+
+  return 0;
+}
+
+ImpulseController::ImpulseController()
+    : nh_("~"), time_step_(0.001), real_time_factor_(1000), update_rate_(10) {
+  // Parameters
+  // Get parameter names
+  nh_.param<std::string>("joint_states_topic", topic_joint_states_,
+                         "/legs/joint_states");
+  nh_.param<std::string>("legs_topic", topic_legs_,
+                         "/legs/leg_controllers/command");
+  nh_.param<std::string>("gazebo_physic_properties_topic",
+                         topic_gazebo_physic_properties_,
+                         "/gazebo/get_physics_properties");
+  nh_.param<std::string>("gazebo_set_model_configuration_topic",
+                         topic_gazebo_set_model_configuration_,
+                         "/gazebo/set_model_configuration");
+  nh_.param<std::string>("gazebo_reset_simulation_topic",
+                         topic_gazebo_reset_simulation_,
+                         "/gazebo/reset_simulation");
+  nh_.param<std::string>("impulse_topic", topic_impulse_, "/legs/impulse");
+
+  // Get update rate from gazebo
+  update_rate_mutex_.lock();
+  rate_.reset(new ros::Rate(update_rate_ / (time_step_ * real_time_factor_)));
+  update_rate_mutex_.unlock();
+
+  update_rate_thread_ =
+      std::thread(&ImpulseController::updateRate,
+                  this);  // We do it in a different thread so we dont
+                          // have to wait to the service call.
+  update_rate_thread_.detach();
+
+  // Publishers
+  pub_legs_ = nh_.advertise<std_msgs::Float64MultiArray>(topic_legs_, 1);
+
+  // Services
+  srv_server_impulse_ = nh_.advertiseService(
+      topic_impulse_, &ImpulseController::callbackServiceImpulse, this);
+
+  srv_client_reset_simulation =
+      nh_.serviceClient<std_srvs::Empty>(topic_gazebo_reset_simulation_);
+  srv_client_gazebo_physic_properties_ =
+      nh_.serviceClient<gazebo_msgs::GetPhysicsProperties>(
+          topic_gazebo_physic_properties_);
+  srv_client_gazebo_set_model_configuration_ =
+      nh_.serviceClient<gazebo_msgs::SetModelConfiguration>(
+          topic_gazebo_set_model_configuration_);
+}
+
+void ImpulseController::step() {
+  // std_msgs::Float64MultiArray
+  //    motors_msg;  // (0) Left ankle, (1) Left knee, (2) Left hip,
+  // (3) Right ankle, (4) Right knee, (5) Right hip
+  // motors_msg.data.resize(6);
+
+  // Publish the message
+  // pub_legs_.publish(motors_msg);
+
+  // Spin and update the rate
+  ros::spinOnce();
+  update_rate_mutex_.lock();
+  rate_->sleep();
+  update_rate_mutex_.unlock();
+}
+
+void ImpulseController::stop() {
+  std_msgs::Float64MultiArray motors_msg;
+  motors_msg.data.resize(6);
+
+  // for (int joint = 0; joint < 6; ++joint) motors_msg.data.at(joint) = 0.0;
+  // pub_legs_.publish(motors_msg);
+  // ROS_WARN("Two Neuron Controller stoped");
+}
+
+void ImpulseController::resetSimulation() {
+  // Resets the simulation
+  ROS_INFO("Reset");
+  std_srvs::Empty reset_simualtion_srv;
+  srv_client_reset_simulation.call(reset_simualtion_srv);
+
+  // Sets initial positions
+  gazebo_msgs::SetModelConfiguration initial_configuration_msg;
+  initial_configuration_msg.request.model_name = "legs";
+  initial_configuration_msg.request.urdf_param_name = "robot_description";
+
+  left_hip_initial_pos_ = 0.67;
+  right_hip_initial_pos_ = 0.67;
+  left_knee_initial_pos_ = -0.8;
+  right_knee_initial_pos_ = -0.8;
+  left_ankle_initial_pos_ = 0.0;
+  right_ankle_initial_pos_ = 0.0;
+
+  initial_configuration_msg.request.joint_names.push_back("left_hip");
+  initial_configuration_msg.request.joint_positions.push_back(
+      left_hip_initial_pos_);
+  initial_configuration_msg.request.joint_names.push_back("left_knee");
+  initial_configuration_msg.request.joint_positions.push_back(
+      left_knee_initial_pos_);
+  //  initial_configuration_msg.request.joint_names.push_back("left_ankle");
+  //  initial_configuration_msg.request.joint_positions.push_back(
+  //      left_ankle_initial_pos_);
+
+  initial_configuration_msg.request.joint_names.push_back("right_hip");
+  initial_configuration_msg.request.joint_positions.push_back(
+      right_hip_initial_pos_);
+  initial_configuration_msg.request.joint_names.push_back("right_knee");
+  initial_configuration_msg.request.joint_positions.push_back(
+      right_knee_initial_pos_);
+  //  initial_configuration_msg.request.joint_names.push_back("right_ankle");
+  //  initial_configuration_msg.request.joint_positions.push_back(
+  //      right_ankle_initial_pos_);
+
+  srv_client_gazebo_set_model_configuration_.call(initial_configuration_msg);
+}
+
+void ImpulseController::updateRate() {
+  if (ros::service::exists(topic_gazebo_physic_properties_,
+                           false)) {  // Checks if gazebo exists
+    gazebo_msgs::GetPhysicsProperties msg;
+    srv_client_gazebo_physic_properties_.call(msg);
+    if (msg.response.time_step != time_step_ ||
+        msg.response.max_update_rate != real_time_factor_) {
+      ROS_INFO_STREAM("Time changed! Real Time Factor: "
+                      << real_time_factor_ << " Time Step: " << time_step_);
+      real_time_factor_ = msg.response.max_update_rate;
+      time_step_ = msg.response.time_step;
+      update_rate_mutex_.lock();
+      rate_.reset(
+          new ros::Rate(update_rate_ / (time_step_ * real_time_factor_)));
+      update_rate_mutex_.unlock();
+    }
+  }
+  std::this_thread::sleep_for(std::chrono::microseconds(50));
+}
+
+void ImpulseController::callbackSubcriberJointState(
+    sensor_msgs::JointState msg) {
+  left_ankle_pos_ = msg.position.at(0);
+  left_hip_pos_ = msg.position.at(1);
+  left_knee_pos_ = msg.position.at(2);
+  right_ankle_pos_ = msg.position.at(3);
+  right_hip_pos_ = msg.position.at(4);
+  right_knee_pos_ = msg.position.at(5);
+}
+
+bool ImpulseController::callbackServiceImpulse(
+    legs_controllers::impulse::Request &req,
+    legs_controllers::impulse::Response &res) {
+  resetSimulation();
+
+  return 1;
+}
--- src/legs_controllers/src/impulse_controller.h
+++ src/legs_controllers/src/impulse_controller.h
@@ -1,9 +1,90 @@
 #ifndef IMPULSE_CONTROLLER_H
 #define IMPULSE_CONTROLLER_H
 
-class impulse_controller {
+// System
+#include <signal.h>
+
+// STD
+#include <vector>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <mutex>
+#include <thread>
+
+// ROS
+#include "ros/ros.h"
+#include "std_msgs/Float64MultiArray.h"
+#include "sensor_msgs/JointState.h"
+#include "gazebo_msgs/GetPhysicsProperties.h"
+#include "gazebo_msgs/SetModelConfiguration.h"
+#include "std_srvs/Empty.h"
+#include "legs_controllers/impulse.h"
+
+class ImpulseController {
  public:
-  impulse_controller();
+  ImpulseController();
+
+  /**
+   * @brief updateRate Updates the frequency at which the controller is updated.
+   * This parameter is got from a default parameter from the launch file and, in
+   * the case of being used with gazebo, from the simulator perse.
+   */
+  void updateRate();
+
+  /**
+   * @brief step
+   */
+  void step();
+
+  /**
+   * @brief stop
+   */
+  void stop();
+
+  /**
+   * @brief restart The simulation and puts the robot in the inital position
+   */
+  void resetSimulation();
+
+  // Callbacks
+  /**
+   * @brief callbackSubcriberJointState
+   * @param msg
+   */
+  void callbackSubcriberJointState(sensor_msgs::JointState msg);
+
+  bool callbackServiceImpulse(legs_controllers::impulse::Request& req,
+                              legs_controllers::impulse::Response& res);
+
+ private:
+  // ROS
+  ros::NodeHandle nh_;
+  ros::Publisher pub_legs_;
+  ros::Subscriber sub_joint_states_;
+  ros::ServiceClient srv_client_gazebo_physic_properties_,
+      srv_client_gazebo_set_model_configuration_, srv_client_reset_simulation;
+
+  ros::ServiceServer srv_server_impulse_;
+
+  std::string topic_legs_, topic_joint_states_, topic_gazebo_physic_properties_,
+      topic_gazebo_set_model_configuration_, topic_gazebo_reset_simulation_,
+      topic_impulse_;
+
+  // Step time
+  double time_step_;
+  double real_time_factor_;
+  double update_rate_;
+  std::shared_ptr<ros::Rate> rate_;
+  std::recursive_mutex update_rate_mutex_;
+  std::thread update_rate_thread_;
+
+  // Controller: common
+  double left_hip_pos_, left_knee_pos_, left_ankle_pos_, right_hip_pos_,
+      right_knee_pos_, right_ankle_pos_;
+
+  double left_hip_initial_pos_, left_knee_initial_pos_, left_ankle_initial_pos_,
+      right_hip_initial_pos_, right_knee_initial_pos_, right_ankle_initial_pos_;
 };
 
 #endif  // IMPULSE_CONTROLLER_H
